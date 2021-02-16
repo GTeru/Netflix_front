8177c4854d1b5a2e5e0b8cc5ef2614158e383d7a
gmteruya <gustavo.teruya@intervalor.com.br>
2021-02-16 18:23:36 -0300

    refactor e ajustes do carossel

--- js/owl/owl_setup.js
+++ js/owl/owl_setup.js
@@ -1,8 +1,17 @@
 $('.owl-carousel').owlCarousel({
     loop:false,
-    nav:false,
+    nav:true,
     dots:false,
     margin:10,
+    navContainerClass:'owl-nav-custom', 
+    navText:[
+        '<span aria-label="' + 'Previous' + '"><i class="fas fa-angle-left"></i></span>',
+        '<span aria-label="' + 'Next' + '"><i class="fas fa-angle-right"></i></span>'
+    ],
+    navClass: [
+        'owl-prev-custom',
+        'owl-next-custom'
+    ],
     responsive:{
         0:{
             items:1
@@ -11,7 +20,7 @@ $('.owl-carousel').owlCarousel({
             items:3
         },
         1000:{
-            items:5
+            items:6
         }
     }
 })
